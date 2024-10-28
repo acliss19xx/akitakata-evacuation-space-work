431394f3a7327b6fdf4b45aa60fb0b8e1f62fc1d
Hirofumi Inoue <acliss19xx@gmail.com>
2024-10-28 21:23:56 +0900

    避難所データ 避難所データ用の「型」を定義した

---
+++ src/types/types.ts
@@ -0,0 +1,21 @@
+export type MarkerData = {
+  name: string;
+  address: string;
+  tel: string;
+  otasuke_phone: string;
+  lat: number;
+  long: number;
+  capacity: number;
+  updated_at: string;
+};
+
+export type EvacuationSpace = {
+  name: string;
+  address: string;
+  tel: string;
+  otasuke_phone: string;
+  latitude: number;
+  longitude: number;
+  capacity: number;
+  updated_at: string;
+};
